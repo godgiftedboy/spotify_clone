eddf510cd19f2a161c788942d468ed0bba2f0995
rajprajapati12321 <rajprajapati@redwire.com.np>
2024-08-28 11:22:44 +0545

    chore: firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:spotify/core/theme.dart';
 import 'package:spotify/features/auth/presentation/logic/auth_controller.dart';
 import 'package:spotify/features/auth/presentation/views/screens/login_page.dart';
 import 'package:spotify/features/home/presentation/views/screens/home_page.dart';
+import 'package:spotify/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   final container = ProviderContainer();
   await container.read(authControllerProvider.notifier).initSharedPreferences();
   await container.read(authControllerProvider.notifier).checkLogin();
